a84febe55ac0f37c1709a39d6bbd97e4fbb02871
Jongmin Lee <starjongmin@gmail.com>
2020-11-24 22:08:20 +0900

    update script

--- env_install.bat
+++ env_install.bat
@@ -1,11 +1,11 @@
-conda env create -f environment.yml
-conda activate rl
+@REM conda env create -f environment.yml
+@REM conda activate rl
 
-rmdir tmp /s /q
-mkdir tmp
+rmdir tmp /s /q 2>nul 
+mkdir tmp 
 cd tmp
-curl.exe https://codeload.github.com/benelot/pybullet-gym/zip/master --output pybullet-gym.zip
-unzip pybullet-gym.zip
+curl.exe https://codeload.github.com/benelot/pybullet-gym/zip/master --output pybullet-gym-master.zip
+unzip pybullet-gym-master.zip
 cd pybullet-gym-master
 pip install -e .
 cd ..\..
--- env_install.ps1
+++ env_install.ps1
@@ -1,11 +1,11 @@
-conda env create -f environment.yml
-conda activate rl
+# conda env create -f environment.yml
+# conda activate rl
 
 rm tmp -ea ig -Force -Recurse
 mkdir tmp
 cd tmp
-wget https://github.com/benelot/pybullet-gym/archive/master.zip -OutFile pybullet-gym.zip
-unzip pybullet-gym.zip
+wget https://codeload.github.com/benelot/pybullet-gym/zip/master -OutFile pybullet-gym-master.zip
+unzip pybullet-gym-master.zip
 cd pybullet-gym-master
 pip install -e .
 cd ..\..
